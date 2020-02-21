bb2399383f29248c5c8c1c8e3de3e93b93c26072
DMG <dmwaigithinji@gmail.com>
2020-02-21 18:31:48 +0300

    Mainheader

--- src/App/Layout/MainHeader/index.js
+++ src/App/Layout/MainHeader/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import logo from '../../../assets/images/header/logo.png';
+import phone from '../../../assets/images/header/phone-icon.png';
+
 const MainHeader = () => {
 
     return (
         <div>
-            <h3>Vacay Flights</h3>
+            <nav className= "navbar main-header navbar-light" >
+            <img id="main-logo" src={logo} alt="logo" className="logo" />
+                <div className= "d-flex inline  align-items-center" >
+                <img id="main-logo" src={phone} alt="phone" height="25" width="30" className= "pr-2"/> &nbsp;
+                    <div className= "d-flex flex-column" >
+                        <span className="semi-bold">Contact</span>
+                        <span>0712345678</span>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
