8915c32418622ae82bc7b2aaea1931a6d61b8c97
DMG <dmwaigithinji@gmail.com>
2020-02-23 15:11:57 +0300

    Remove auth_id

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,5 @@
 export default {
-    auth_id: "10",
-    // auth_id: "10201676-E2B7-4AEE-8924-DD42E1FD87B0",
-    auth_password: "4xi@Is&6$oit",
+    auth_id: "",
+    auth_password: "",
     'Content-Type': 'application/json',
 }
